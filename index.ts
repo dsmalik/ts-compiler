5013bf0ce52d514cb9cc6ab2c58396daa4a068b0
Dinesh Singh <dinesh2424@gmail.com>
2021-11-13 13:51:54 +0530

    Cleaned code to focus on problem

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { ImportDeclaration, ImportSpecifier, LanguageService, Project, SourceFile } from 'ts-morph';
+import { ImportDeclaration, ImportSpecifier, Project, SourceFile } from 'ts-morph';
 import * as ts from 'typescript';
 
 const project = new Project({
@@ -8,7 +8,6 @@ const project = new Project({
 project.addSourceFilesAtPaths('src/**/*.ts');
 
 const sourceFiles = project.getSourceFiles();
-const typeChecker = project.getTypeChecker();
 
 console.log('Total Source files - ', sourceFiles.length);
 
@@ -31,7 +30,6 @@ const getReferencedFiles = (filePath: string): string[] => {
 };
 
 const languageService = project.getLanguageService();
-const basePath = '.';
 const imports: Map<string, string> = new Map<string, string>();
 
 let i = 1;
@@ -64,31 +62,30 @@ const getImportsWithAbsolutePaths = (sourceFile: SourceFile, importDeclaration: 
 
 		if (imports.has(importName)) {
 			fileImports.set(importName, imports.get(importName)!);
-			console.log(`  Cached Import - ${getImportInfo(ni)}`);
+			console.log(`  TSMORPH - Cached Import - ${getImportInfo(ni)}`);
 		} else {
 			const definitions = languageService.getDefinitionsAtPosition(sourceFile, ni.getStart());
 
 			definitions.forEach(def => {
 				if (imports.has(importName)) {
 					fileImports.set(importName, imports.get(importName)!);
-					console.log(`  Cached Import - ${ni.getName()} - ${imports.get(ni.getName())}`);
+					console.log(`  TSMORPH - Cached Import - ${ni.getName()} has path - ${imports.get(ni.getName())}`);
 				} else {
-					console.log(`     Import - ${ni.getName()} - ${def.getSourceFile().getFilePath()}`);
+					console.log(
+						`    TSMORPH - Import - ${ni.getName()} has path - ${def.getSourceFile().getFilePath()}`
+					);
 					let importAbsolutePath = '';
 					const sourceDirectoryPath = def.getSourceFile().getDirectoryPath();
 					if (sourceDirectoryPath.indexOf('node_modules/') > 0) {
 						importAbsolutePath =
-							sourceDirectoryPath.substring(sourceDirectoryPath.indexOf('node_modules/') + 13) +
-							def.getSourceFile().getBaseNameWithoutExtension();
+							sourceDirectoryPath + '/' + def.getSourceFile().getBaseNameWithoutExtension();
 
-						console.log(`  Import path - ${importName} = ${importAbsolutePath}`);
+						//console.log(`  Import path - ${importName} = ${importAbsolutePath}`);
 					} else {
 						importAbsolutePath =
-							sourceDirectoryPath.substring(sourceDirectoryPath.indexOf('/src') + 1) +
-							'/' +
-							def.getSourceFile().getBaseNameWithoutExtension();
+							sourceDirectoryPath + '/' + def.getSourceFile().getBaseNameWithoutExtension();
 
-						console.log(`  Import path - ${importName} = ${importAbsolutePath}`);
+						//console.log(`  Import path - ${importName} = ${importAbsolutePath}`);
 					}
 
 					imports.set(importName, importAbsolutePath);
@@ -99,41 +96,26 @@ const getImportsWithAbsolutePaths = (sourceFile: SourceFile, importDeclaration: 
 	});
 
 	return fileImports;
-	// // // // // importDeclaration.remove();
 };
-/*
-const getInformation = (filePath: string, ignoreBasePath: boolean = true): [imports: Map<string, string>, sf: SourceFile] => {
-	const fileImports: Map<string, string> = new Map<string, string>();
 
-	const sourceFile = project.addSourceFileAtPath(filePath);
-	const currentFilePath = sourceFile.getFilePath();
-	console.log(`${i++} - Source file to scan - ${sourceFile.getFilePath()}`);
+// Import that we changing to absolute path and need to track so that we can update the symbols
+let importNamesToUpdateSymbolFor: string[] = [];
 
-	
-
-	sourceFile.getImportDeclarations().forEach((importDeclaration) => {
-		//return handleImportDeclaration(importDeclaration);
-		
-	});
-
-	// // // // // fileImports.forEach((v, k) => {
-	// // // // // 	if (!v) {
-	// // // // // 		console.log(`${currentFilePath} - Could not map import - ${k}`);
-	// // // // // 	}
-
-	// // // // // 	sourceFile.insertStatements(0, `import {${k}} from '${v}';`);
-	// // // // // });
-
-	return [fileImports, sourceFile];
-};
-*/
 export const factory = (/**/) => {
 	function visitor(ctx: ts.TransformationContext, sf: ts.SourceFile) {
 		console.log('============Visiting file - ', sf.fileName);
-
 		const sourceFileToScan = project.addSourceFileAtPath(sf.fileName);
 
 		const visitor: ts.Visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
+			// if (ts.isIdentifier(node)) {
+			// 	// For an import with name - getName, it transpiles to getName_1 in require syntax, so the identifier becomes getName_1.getName
+			// 	// So doing it manually here instead of changing the symbol which i am looking how to properly?
+			// 	const newNode = ts.createIdentifier(
+			// 		`${importNamesToUpdateSymbolFor.includes(node.text) ? `${node.text}_1.${node.text}` : node.text}`
+			// 	);
+			// 	return newNode;
+			// }
+
 			if (ts.isImportDeclaration(node)) {
 				console.log('  ### Import declaration -', node.getText());
 
@@ -144,75 +126,44 @@ export const factory = (/**/) => {
 
 					const newImports: Map<string, Array<string>> = new Map<string, Array<string>>();
 
-					for (const i of importsToChange) {
-						console.log(`   $$$$$$ Import ${i[0]} - ${i[1]}`);
-						if (newImports.has(i[1])) {
-							newImports.set(i[1], [...newImports.get(i[1])!, i[0]]);
+					for (const importToChange of importsToChange) {
+						importNamesToUpdateSymbolFor.push(importToChange[0]);
+						console.log(`   $$$$$$ Import ${importToChange[0]} - ${importToChange[1]}`);
+						if (newImports.has(importToChange[1])) {
+							newImports.set(importToChange[1], [
+								...newImports.get(importToChange[1])!,
+								importToChange[0]
+							]);
 						} else {
-							newImports.set(i[1], [i[0]]);
+							newImports.set(importToChange[1], [importToChange[0]]);
 						}
 					}
 
 					const importDeclarationToAdd: ts.ImportDeclaration[] = [];
-					for (const j of newImports) {
+					for (const newImport of newImports) {
 						const importSpecifiers: ts.ImportSpecifier[] = [];
-						console.log(`   $$$$$$$$ Import path - ${j[0]}`);
-						j[1].forEach(imp => {
-							console.log('      $$$$$$$$ Import name - ', imp);
+						newImport[1].forEach(imp => {
 							importSpecifiers.push(ts.createImportSpecifier(undefined, ts.createIdentifier(imp)));
 						});
 
 						if (importSpecifiers.length) {
-							importDeclarationToAdd.push(
-								ts.createImportDeclaration(
-									undefined,
-									undefined,
-									ts.createImportClause(undefined, ts.createNamedImports(importSpecifiers)),
-									ts.createLiteral(j[0] + '123')
-								)
+							const d = ts.createImportDeclaration(
+								undefined,
+								undefined,
+								ts.createImportClause(undefined, ts.createNamedImports(importSpecifiers)),
+								ts.createLiteral(newImport[0])
 							);
+							importDeclarationToAdd.push(d);
 						}
 					}
 
 					console.log('############################################');
+					// Update the Current node by sending new imports
 					return [...importDeclarationToAdd];
-					// Update the Current node
 				} else {
 					console.log('  ### Could not find import declaration for - ', node.getText());
 				}
 			}
-
-			console.log(` Kind = ${ts.SyntaxKind[node.kind]} - ${node.getText()}`);
-			/*
-			if (ts.isSourceFile(node) && node.getSourceFile().fileName.indexOf('app.spec') > 0) {
-				console.log('  ###Source file node for -', node.fileName);
-				// const [allNamedImports, sf1] = getInformation(node.fileName, false);
-
-				// const n = ts.updateSourceFileNode(sf, [
-				// 	ts.createImportDeclaration(
-				// 		undefined,
-				// 		undefined,
-				// 		ts.createImportClause(
-				// 			undefined,
-				// 			ts.createNamedImports([
-				// 				ts.createImportSpecifier(undefined, ts.createIdentifier('getName1')),
-				// 				ts.createImportSpecifier(undefined, ts.createIdentifier('getName')),
-				// 			])
-				// 		),
-				// 		ts.createLiteral('./libs132')
-				// 	),
-				// 	// Ensures the rest of the source files statements are still defined.
-				// 	...sf.statements,
-				// ]);
-
-				// console.log('');
-				// console.log('New Node is - ', ts.createPrinter().printFile(n));
-				// console.log('');
-
-				// return ts.visitEachChild(n, visitor2, ctx);
-				return node;
-			}
-*/
 
 			try {
 				return ts.visitEachChild(node, visitor, ctx);
@@ -226,11 +177,18 @@ export const factory = (/**/) => {
 
 	return (ctx: ts.TransformationContext): ts.Transformer<ts.SourceFile> => {
 		return (sf: ts.SourceFile) => {
+			let newS: ts.SourceFile = sf;
+
 			if (sf.fileName.indexOf('app.spec') > 0) {
 				try {
-					return ts.visitNode(sf, visitor(ctx, sf));
+					newS = ts.visitNode(sf, visitor(ctx, sf));
+					return newS;
 				} finally {
-					console.log('   ________________File updated... -', sf.getFullText());
+					console.log('        ###   Symbols to change -', importNamesToUpdateSymbolFor);
+					console.log('Updated source is - ');
+					console.log(ts.createPrinter().printFile(newS));
+					// console.log('   ________________File updated... -', newS.getFullText());
+					// console.log(' Import Symbol to update -', importNamesToUpdateSymbol);
 				}
 			}
 
@@ -238,48 +196,3 @@ export const factory = (/**/) => {
 		};
 	};
 };
-
-// sourceFiles.forEach((s) => {
-// 	const sourceFile = getInformation(s.getSourceFile().getFilePath(), false);
-
-// 	console.log('Source file - ', sourceFile.print());
-// });
-// 	// console.log('Checking File - ', s.getSourceFile().getFilePath());
-// 	// s.getImportDeclarations().forEach((si) => {
-// 	// 	console.log('  Full import text - ', si.getText());
-// 	// 	// const l: LanguageService = LanguageService.;
-// 	// 	console.log('    Imported member -', si.getImportClause()?.getText());
-// 	// 	// si.getModuleSpecifierSourceFile()?.getSourceFile().getImportDeclaration("getName");
-// 	// 	si.getModuleSpecifierSourceFile()
-// 	// 		?.getReferencedSourceFiles()
-// 	// 		.forEach((refFile) => {
-// 	// 			console.log('      Referenced file - ', refFile.getFilePath());
-// 	// 		});
-// 	// 	console.log();
-// 	// 	si.getNamedImports().forEach((namedImport) => {
-// 	// 		console.log('   Named import -', namedImport.getName());
-// 	// 		// console.log('ok  ', namedImport.getImportDeclaration().getSourceFile().getFilePath());
-// 	// 		// namedImport.getImportDeclaration().gets
-// 	// 		const a = namedImport.getSymbol()?.getAliasedSymbol();
-// 	// 		const exports = typeChecker.getDeclaredTypeOfSymbol(a!);
-// 	// 		console.log();
-// 	// 		namedImport
-// 	// 			.getType()
-// 	// 			.getSymbol()
-// 	// 			?.getDeclarations()
-// 	// 			.map((d) => d.getSourceFile())
-// 	// 			.forEach((sf) => {
-// 	// 				console.log('       Referenced Path is - ', sf.getFilePath());
-// 	// 			});
-// 	// 	});
-// 	// 	// console.log('   Imports -', s.getText());
-// 	// 	// console.log('      -----', s.getFullText());
-// 	// });
-// 	// s.getReferencedSourceFiles().forEach(s => {
-// 	//   console.log('   Depends on -', s.getFilePath());
-// 	// });
-// 	// s.getReferencingSourceFiles().forEach(s => {
-// 	//   console.log('   Used by -', s.getFilePath());
-// 	// });
-// 	// console.log();
-// });
